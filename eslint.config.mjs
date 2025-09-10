2b23617f54219c905658325dc6a3d4636be20e63
lylapitajen <lylapitajen00@gmail.com>
2025-09-10 17:10:00 +0100

    ?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,15 +5,26 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    extends: [js.configs.recommended, pluginReact.configs.flat.recommended],
     rules: {
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
       "no-unused-vars": "warn",
     },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
-  pluginReact.configs.flat.recommended,
 ]);
